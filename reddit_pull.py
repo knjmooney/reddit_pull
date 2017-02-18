6e6beebf069f11cd8e3758dfab27f85846513770
Kevin Mooney <knjmooney@gmail.com>
2017-02-18 23:51:32 +0000

    Added default argument for url

--- reddit_pull.py
+++ reddit_pull.py
@@ -4,19 +4,24 @@ from pprint import pprint
 import requests
 import json
 import argparse
+import urlparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--url', help='url of subreddit')
 args = parser.parse_args()
 
-r = requests.get(args.url + '/.json',
-                 headers = {'User-agent': 'your bot 0.1'})
+# Make the construction of the path more intuitive
+subreddit = 'https://www.reddit.com/'
+if args.url:
+    subreddit = args.url
+response = requests.get(subreddit + '.json',
+                        headers = {'User-agent': 'your bot 0.1'})
 
-if r.ok:
+if response.ok:
     print "Download successful"
 else:
-    print "Error ", r.status_code
-    exit(r.status_code)
+    print "Error ", response.status_code
+    exit(response.status_code)
 
-data = r.json()
+data = response.json()
 print data['data']['children'][0]['data']['title']
