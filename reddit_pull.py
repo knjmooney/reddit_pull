43320af040c6b376ae136c156a791808529e95ae
frnchambers <frnchambers@gmail.com>
2017-02-28 21:06:32 +0100

    change the url reading so only reuqire subreddit name

--- reddit_pull.py
+++ reddit_pull.py
@@ -8,14 +8,18 @@ import urlparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--url', help='url of subreddit')
+
 args = parser.parse_args()
 
 # Make the construction of the path more intuitive
 subreddit = 'https://www.reddit.com/'
+
 if args.url:
-    subreddit = args.url
+    subreddit = "https://www.reddit.com/r/" + args.url + "/"
+
 response = requests.get(subreddit + '.json',
                         headers = {'User-agent': 'your bot 0.1'})
+
 
 if response.ok:
     print "Download successful"
