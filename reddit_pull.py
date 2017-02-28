fbb93034513225c6a7f89d10f247662068f948db
Kevin Mooney <knjmooney@gmail.com>
2017-02-28 22:00:10 +0000

    Added extra cmdline arguments

--- reddit_pull.py
+++ reddit_pull.py
@@ -7,29 +7,29 @@ import argparse
 import urlparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--subreddit', help='name of subreddit')
+parser.add_argument('--subreddit', help='Name of subreddit', default='popular')
+parser.add_argument('--filename' , help='File to print output to',
+                    default='pull_output.txt')
+parser.add_argument('--number' , help='Number of files to download',
+                    default=1, type=int)
 
 args = parser.parse_args()
 
-# Make the construction of the path more intuitive
-subreddit_url = 'https://www.reddit.com/'
+# Set the subreddit variable
+subreddit_url = "https://www.reddit.com/r/" + args.subreddit + '/'
 
-if args.subreddit:
-    subreddit_url = "https://www.reddit.com/r/" + args.subreddit + '/'
-
-response = requests.get(urlparse.urljoin(subreddit_url, '.json'),
+# Request page from reddit
+response = requests.get(subreddit_url + '.json',
                         headers = {'User-agent': 'your bot 0.1'})
-
-
-if response.ok:
-    print "Download successful from", urlparse.urljoin(subreddit_url, '.json')
-else:
+if not response.ok:
+    # print "Download successful from", urlparse.urljoin(subreddit_url, '.json')
+# else:
     print "Error ", response.status_code
     exit(response.status_code)
 
+# Parse and print to file
 data = response.json()
-
-# pprint(data['data']['children'][0]['data'])
-
-for i in range(0,3):
-    print data['data']['children'][i]['data']['title']
+output_filehandle = open(args.filename, 'w+')
+for i in range(0,args.number):
+    # print >> output_filehandle, data['data']['children'][i]['data']['title']
+    print data['data']['children'][i]['data']['title'],'\n'
