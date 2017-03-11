738709beef019eb35aace6d00855df158630f8e4
Kevin Mooney <knjmooney@gmail.com>
2017-03-11 18:59:18 +0000

    Tidied up the python script

--- reddit_pull.py
+++ reddit_pull.py
@@ -5,31 +5,58 @@ import json
 import requests
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--subreddit', help='Name of subreddit', default='popular')
-parser.add_argument('--number' , help='Number of files to download',
-                    default=1, type=int)
-parser.add_argument('--filename' , help='File to print output to')
+# This function both sets up the command line arguments and then
+# parses them before printing to screen
+def rp_parse_command_line():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--subreddit', help='Name of subreddit', default='popular')
+    parser.add_argument('--number' , help='Number of files to download',
+                        default=1, type=int)
+    parser.add_argument('--filename' , help='File to print output to')
 
-args = parser.parse_args()
+    return parser.parse_args()
 
-# Set the subreddit variable
-subreddit_url = "https://www.reddit.com/r/" + args.subreddit + '/'
+# Returns a list of the N (N = number_to_download) top titles in
+# descending order from subreddit_name.
+def rp_get_list_of_subreddit_titles(subreddit_name, number_to_download):
+    # Set the subreddit variable
+    subreddit_url = "https://www.reddit.com/r/" + subreddit_name + '/'
 
-# Request page from reddit
-response = requests.get(subreddit_url + '.json',
+    # Request page from reddit
+    response = requests.get(subreddit_url + '.json',
                         headers = {'User-agent': 'your bot 0.1'})
 
-if not response.ok:
-    print "Error ", response.status_code
-    exit(response.status_code)
+    if not response.ok:
+        print "Error ", response.status_code
+        exit(response.status_code)
 
-# Parse and print to file
-data = response.json()
-if args.filename is not None:
-    output_filehandle = open(args.filename, 'w+')
-else:
-    output_filehandle = sys.stdout
+    # Parse and print to file
+    data = response.json()
+    subreddit_titles = list()
 
-for i in range(0,args.number):
-    print >> output_filehandle, data['data']['children'][i]['data']['title'], '\n'
+    for i in range(0,number_to_download):
+        subreddit_titles.append(data['data']['children'][i]['data']['title'])
+
+    return subreddit_titles
+
+# Main routine, Similar to C/C++, this is the first function run on
+# start up of the script.
+def main():
+
+    args = rp_parse_command_line()
+
+    subreddit_titles = rp_get_list_of_subreddit_titles(args.subreddit,
+                                                       args.number)
+
+    if args.filename is not None:
+        output_filehandle = open(args.filename, 'w+')
+    else:
+        output_filehandle = sys.stdout
+
+    for i in range(0,args.number):
+        print >> output_filehandle, subreddit_titles[i], '\n'
+
+# This is to stop main being run if script is imported into an
+# interactive session. (Well I think that's what it does)
+if __name__ == '__main__':
+    main()
